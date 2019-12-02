0d045a57f5ddae4e41fd0887fe804ffc23ed34df
vampir490 <victornik90@gmail.com>
2019-12-02 21:35:09 +0300

    Added index with uniqueness to :users :username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_29_193849) do
+ActiveRecord::Schema.define(version: 2019_12_02_183028) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_11_29_193849) do
     t.string "password_hash"
     t.string "password_salt"
     t.string "avatar_url"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
