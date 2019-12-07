c6b2c17a57e07631fcb31d60d91b8b4c30792105
vampir490 <victornik90@gmail.com>
2019-12-07 12:40:34 +0300

    users avatar background

---
+++ db/migrate/20191207090823_add_credentials_to_users.rb
@@ -0,0 +1,5 @@
+class AddCredentialsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :background_color, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_183028) do
+ActiveRecord::Schema.define(version: 2019_12_07_090823) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_12_02_183028) do
     t.string "password_hash"
     t.string "password_salt"
     t.string "avatar_url"
+    t.string "background_color"
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
